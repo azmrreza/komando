11021659d40e426fee5cac3b8ee35bfff9e385cd
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-02 10:58:56 -0800

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'nod32' then -- Put everything you like :)
+  if matches[1]:lower() == 'virus' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /nod32 ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /virus ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
