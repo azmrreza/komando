fc36dd8117dd9dfe003762293b5c32ecb11c1792
amin123amin <amin.ilamboy14@gmail.com>
2016-03-03 15:05:21 -0800

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -244,7 +244,7 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {145266153},--Sudo users
+	    sudo_users = {127651068},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
